9a45d04a4f8ff62083770a422dd8ddfc73ae2c2a
Allison Farris <allison.perry98@gmail.com>
2023-11-26 21:00:06 -0700

    added login mutation and type book

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,45 +1,46 @@
 
-const { gql } = require('apollo-server-express');
+const { gql } = require('graphql-tag');
 
 const typeDefs = gql`
+input bookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+}
   type User {
     _id: ID!
     username: String!
     email: String!
     bookCount: Int!
-    savedBooks: [Book]!
+    savedBooks: [Book]
   }
 
   type Book {
     bookId: String!
-    authors: [String]!
-    description: String!
-    title: String!
+    authors: [String]
+    description: String
+    title: String
     image: String
     link: String
   }
 
   type Auth {
-    token: String!
-    user: User!
+    token: ID!
+    user: User
   }
 
   type Query {
-    me: User
+    user: User
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(
-      authors: [String]!
-      description: String!
-      title: String!
-      bookId: String!
-      image: String
-      link: String
-    ): User
-    removeBook(bookId: String!): User
+    saveBook(bookToSave: bookInput): User
+    removeBook(bookId: ID!): User
   }
 `;
 
